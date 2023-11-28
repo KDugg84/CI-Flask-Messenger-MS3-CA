c70951f09b9689a27d55e6cfa830380ceddb83d8
KDugg84 <>
2023-11-28 10:27:53 +0000

    Add routes for base, home and about pages

--- flaskmessenger/routes.py
+++ flaskmessenger/routes.py
@@ -1,7 +1,34 @@
 from flask import render_template
 from flaskmessenger import app, db
 
+# Dummy date to iterate through using jinja for loops.
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Message 1',
+        'content': 'First post content',
+        'date_posted': 'November 27th, 2023'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Message 2',
+        'content': 'Second post content',
+        'date_posted': 'November 27th, 2023'
+    }
+]
+
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return render_template("base.html")
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
