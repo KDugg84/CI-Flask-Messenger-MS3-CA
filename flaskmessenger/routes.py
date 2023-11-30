64baaecda312a504175be77cd969ee6e90621e2d
KDugg84 <>
2023-11-30 11:15:18 +0000

    Import url_for as redirect argument

--- flaskmessenger/routes.py
+++ flaskmessenger/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flaskmessenger import app, db
 
